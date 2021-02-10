98f1c55aca623e879a7c53268d82a706111ff90e
Luiz Max Carvalho <lmcarvalho@ufrj.br>
2021-02-10 19:30:31 -0300

    Implemented split-delta

--- R/performance_metrics.R
+++ R/performance_metrics.R
@@ -56,13 +56,57 @@ scaled_alpha <- function(samples, k = 1, v = 1, p = NULL){
 #' @details This index is similar to \code{\link[BinaryMarkovChains]{scaled_average_transitions}} but
 #'  does not require any knowledge about any of the Markov chain parameters.
 #'  On the other hand, it is computed **conditional** on the observed occupation time and thus inherits its uncertainty.
-#' @seealso  \code{\link[BinaryMarkovChains]{MC_neff}} \code{\link[BinaryMarkovChains]{scaled_alpha}} \code{\link[BinaryMarkovChains]{scaled_average_transitions}}
+#' @seealso  \code{\link[BinaryMarkovChains]{split_switching_ratio}} 
+#' \code{\link[BinaryMarkovChains]{MC_neff}} \code{\link[BinaryMarkovChains]{scaled_alpha}}
+#'  \code{\link[BinaryMarkovChains]{scaled_average_transitions}}
+#'  @examples 
+#' X1 <- c(0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0)
+#' X2 <- c(0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0)
+#' switching_ratio(X1)
+#' switching_ratio(X2)
+#' switching_ratio(X1[1:8])
+#' switching_ratio(X1[9:16])
+#' switching_ratio(X2[1:8])
+#' switching_ratio(X2[9:16])
 switching_ratio <- function(samples){
   N <- length(samples)
   X.0 <- as.character(samples[1])
   occ.time <- sum(samples)
   max.transitions <- get_maxK(S = occ.time, M = N, X0 = X.0)
-  Sy <- sum(form_Y(samples))
+  Sy <- sum(form_Y(samples)) 
   delta <- Sy/max.transitions
+  if(max.transitions <= 0) delta <- 0
   return(delta)
+}
+
+#' Split-Scaled number of state changes
+#'
+#' Computes the ratio between the number of observed state changes and
+#'  the maximum possible number of state changes given the observed occupation time in 
+#'  a number \code{k} of windows.
+#' @param samples a collection of N samples from a two-state Markov chain.
+#' @param  k a number of windows into which to break up the samples.
+#' Default is \code{k=2}.
+#'
+#' @return a list of scaled number of state transitions (between 0 and 1) in each of the \code{k} windows.
+#' @export split_switching_ratio
+#' @seealso  \code{\link[BinaryMarkovChains]{switching_ratio}} \code{\link[BinaryMarkovChains]{MC_neff}}
+#'  \code{\link[BinaryMarkovChains]{scaled_alpha}} \code{\link[BinaryMarkovChains]{scaled_average_transitions}}
+#' @details This function breaks up the samples into \code{k} windows computes \code{switching_ratio} for
+#' each one. Works in the same spirit as split-Rhat and other
+#'  metrics which compare one part of the chain to the others.
+#' @examples 
+#' X <- rbinom(1E6, size = 1, prob = .2)
+#' split_switching_ratio(X, k = 2)
+#' split_switching_ratio(X, k = 11)
+#' plot(split_switching_ratio(X, k = 200), type = "l")
+split_switching_ratio <- function(samples, k = 2){
+  if(k <= 0) stop("k needs to be larger than 0")
+  if(k >= length(samples)) stop("k needs to be smaller than the number of samples")
+  broken.up <- split(samples, cut(seq_along(samples), k, labels = FALSE))
+  raw.deltas <- lapply(broken.up, switching_ratio)
+  deltas <- unlist(raw.deltas)
+  ns <- unlist(lapply(broken.up, length))
+  names(deltas) <- paste0("n=", ns)
+  return(deltas)
 }
