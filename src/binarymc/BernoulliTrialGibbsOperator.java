81fff84176731dd7b894fc32d695c9a95513a8a8
rbouckaert <higgs.ml@gmail.com>
2021-07-19 12:56:48 +1200

    add flipping order input to BernoulliTrialGibbsOperator

--- src/binarymc/BernoulliTrialGibbsOperator.java
+++ src/binarymc/BernoulliTrialGibbsOperator.java
@@ -1,5 +1,7 @@
 package binarymc;
 
+import java.util.Arrays;
+
 import beast.core.Description;
 import beast.core.Input;
 import beast.core.Input.Validate;
@@ -11,16 +13,25 @@ import beast.util.Randomizer;
 @Description("Bernoulli trial for randomly selected bit in an array.")
 public class BernoulliTrialGibbsOperator extends Operator {
     final public Input<Double> pSuccessInput = new Input<>("p", "probability of success for a", 0.5);
+    final public Input<Boolean> randomOrderInput = new Input<>("randomOrder", "if true, randomly select a dimension, otherwise circle through dimensions deterministically", false);
 
     final public Input<BooleanParameter> parameterInput = new Input<>("parameter", "the parameter to operate a flip on.", Validate.REQUIRED);
 
-    double pSuccess;
-    BooleanParameter p;
+    private double pSuccess;
+    private boolean randomOrder;
+    private boolean [] done;
+    private int current = 0;
+    private BooleanParameter p;
     
     @Override
 	public void initAndValidate() {
     	pSuccess = pSuccessInput.get();
     	p = parameterInput.get(this);
+    	randomOrder = randomOrderInput.get();
+    	if (!randomOrder) {
+    		done = new boolean[p.getDimension()];
+    	}
+    	
     }
 
     /**
@@ -33,7 +44,10 @@ public class BernoulliTrialGibbsOperator extends Operator {
 
         final int dim = p.getDimension();
 
-        final int pos = Randomizer.nextInt(dim);
+        final int pos = randomOrder ? 
+        		Randomizer.nextInt(dim) :
+        			next(dim);
+//        			current++ % dim;
 
         if (Randomizer.nextDouble() < pSuccess) {
             p.setValue(pos, true);
@@ -44,5 +58,22 @@ public class BernoulliTrialGibbsOperator extends Operator {
         // make sure it is always accepted
         return Double.POSITIVE_INFINITY;
     }
+
+	private int next(int dim) {
+		if (current == dim) {
+			Arrays.fill(done, false);
+			current = 0;
+		}
+		current++;
+		int i = Randomizer.nextInt(dim);
+		while (done[i]) {
+			i++;
+			if (i == dim) {
+				i = 0;
+			}
+		}
+		done[i] = true;
+		return i;
+	}
 }
 
