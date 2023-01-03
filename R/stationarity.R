946f23dc0d67140426d625ae4a5be890d6818079
Luiz Max Carvalho <luizepidemiologia@gmail.com>
2023-01-03 17:25:35 -0300

    implementing stationarity stuff; solves #8

--- R/stationarity.R
+++ R/stationarity.R
@@ -30,36 +30,82 @@ split_counts <- function (samples, k = 2)
   rownames(ns) <- c("0", "1")
   return(ns)
 }
-get_transitions_windows <- function(seq, nw = 2){
-  if (nw <= 0) 
-    stop("nw needs to be larger than 0")
-  if (nw >= length(seq)) 
-    stop("nw needs to be smaller than the number of samples")
-  states <- sort(unique(seq))
-  nstates <- length(seq)
-  broken.up <- split(seq, cut(seq_along(seq), nw, labels = FALSE))
-  raw <- lapply(broken.up, get_contingency_array, 1)
-  out <- array(NA, dim = c(nstates, nstates, nw))
-  for(k in 1:nw) out[,, k] <- raw[[k]]
-  return(out)
+#' Row-normalise a matrix with non-negative entries
+#'
+#' @param mat a square matrix with non-negative entries.
+#'
+#' @return a row-stochastic matrix
+#' @export norm_matrix
+#'
+#' @examples
+#' M <- matrix(c(124, 3, 16,
+#'         6, 109, 14,
+#'         22, 9, 142),
+#'       ncol = 3, nrow = 3, byrow = TRUE)
+#'norm_matrix(M)
+norm_matrix <- function(mat){
+  t(apply(mat, 1, function(x) x/sum(x)))
 }
-compute_LRS <- function(seq, nw = 2){
-  states <- sort(unique(seq))
+#'  Compute the likelihood ratio statistic comparing a stationary Markov chain model for \code{seq} to a nonstationary model
+#'
+#' @param seq a vector containing observations from an stochastic process.
+#' @param L an integer specifying how many windows will be compared
+#' @param states the possible states of the Markov chain. If \code{NULL},
+#' will be deduced from 'seq'.
+#' @param eps a small number to avoid zero probabilities
+#'
+#' @return a likelihood ratio statistic comparing H1 = not stationary vs
+#'  H0 = stationary.
+#' @export compute_Hsquared
+#' @examples
+#' X <- rbinom(n = 1E5, size = 1, prob = 0.8)
+#' compute_Hsquared(X)
+compute_Hsquared <- function (seq, L = 10, states = NULL, eps = 1E-17) 
+{
+  ## page 190 of Thomas (2013)
+  ##  "Some Simplifying Conditions for Markov Chain Modeling"
+  if(is.null(states)) states <- sort(unique(seq))
   nstates <- length(states)
-  dt <-  get_transitions_windows(seq, nw)
-  stats <- c()
-  for(k in 1:nw){
-    for( i in 1:nstates){
+  csize <- length(seq)/L
+  windows <- split(seq, ceiling(seq_along(seq)/csize))
+  dt <- ps <- array(NA, dim = c(nstates, nstates, L))
+  for(t in 1:L){
+    dt[,, t] <- get_contingency_array(windows[[t]], 1, states = states)
+    ps[,, t] <- norm_matrix(dt[,, t])
+  }
+  pooledCounts <- apply(dt, c(1, 2), sum)
+  pooledP <- norm_matrix(pooledCounts)
+  Lsep <- 0
+  Lpool <- 0
+  for(t in 1:L){
+    for(i in 1:nstates){
       for(j in 1:nstates){
-        nijk <-  dt[i , j, k] + 1E-17
-        nijp <- sum(dt[i, j, ]) + 1E-17
-        npjk <- sum(dt[, j, k]) + 1E-17
-        npjp <- sum(dt[, j, ]) + 1E-17
-        s <- nijk * log((nijk/nijp)/(npjk/npjp))
-        stats <- c(stats, s)
+        Lsep <- Lsep + dt[i, j, t] * log(ps[i, j, t] + eps)
+        Lpool <- Lpool +  dt[i, j, t] * log(pooledP[i, j] + eps)
       }
     }
   }
-  out <- 2*sum(stats)
+  out <- 2*(Lsep - Lpool)
   return(out)
+}
+#' Compute the Bayes factor testing stationarity of a Markov chain.
+#'
+#' @param seq a vector containing observations from an stochastic process.
+#' @param L an integer specifying how many windows will be compared
+#' @param states the possible states of the Markov chain. If \code{NULL},
+#' will be deduced from 'seq'.
+#'
+#' @return the approximate Bayes factor testing H1 = not stationary vs
+#'  H0 = stationary.
+#' @export compute_stationarity_BF
+#'
+#' @examples
+#' X <- rbinom(n = 1E5, size = 1, prob = 0.8)
+#' compute_stationarity_BF(X)
+compute_stationarity_BF <- function(seq, L = 10, states = NULL){
+  n <- length(seq)
+  if(is.null(states)) states <- sort(unique(seq))
+  Hsq <- compute_Hsquared(seq = seq, L = L, states = states)
+  BF <- (Hsq - 2*log(n))/2
+  return(BF)
 }
